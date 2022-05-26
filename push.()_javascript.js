bdd36839b5a7e03656803e91c3a2a6ebc9fee770
Chosen 3rd <42759948+the-chosen-3rd@users.noreply.github.com>
2022-05-25 20:18:59 -0700

    Add files via upload

---
+++ push.()_javascript.js
@@ -0,0 +1,19 @@
+// push.()
+
+// Push is a way to append data to the end of an array    .push()
+
+// very simply put if you want to add somthing to the end of an array use the array's variable name with .push    
+
+var Array_of_names = ["Mitch", "kelly", "doug", "Slim"]
+
+console.log(Array_of_names)
+console.log("_______________________________________________________________")  // seperation between outputs
+
+// to add more name to the end of this array, use push
+
+
+Array_of_names.push("King Michael, Jaeden, Sarah")
+
+
+console.log(Array_of_names)
+
