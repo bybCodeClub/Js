90a87a6894cd2313bd4b113f7c5f45a40bf3c5e7
Chosen 3rd <42759948+the-chosen-3rd@users.noreply.github.com>
2022-05-26 19:53:34 -0700

    Add files via upload

---
+++ return_variables.js
@@ -0,0 +1,15 @@
+// Getting a return value from a function and putting it in a variable 
+
+
+var Return_value_reassigned = null 
+
+
+function Let_add_numbers(num, num2) {
+    return (num + num2 + 33)  
+}
+
+// To store this function value in a variable;
+
+Return_value_reassigned = document.write(Let_add_numbers(3,8))
+
+// Here were assigning the return_value_re*** variable to the let's_add_number function call  
